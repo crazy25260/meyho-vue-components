06d77b39e756d0b1c0a7b9cf4a233740c341c956
zhangrui <zhangrui6@asiainfo.com>
2021-04-08 23:12:42 +0800

    1、创建我的第一个npm 组件。

---
+++ meyho-vue-swipe-menu/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <MeyhoVueSwipeMenu></MeyhoVueSwipeMenu>
+  </div>
+</template>
+
+<script>
+import MeyhoVueSwipeMenu from "@/components/MeyhoVueSwipeMenu";
+export default {
+  name: 'App',
+  components: {
+    MeyhoVueSwipeMenu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- meyho-vue-swipe-menu/src/components/MeyhoVueSwipeMenu.vue
+++ meyho-vue-swipe-menu/src/components/MeyhoVueSwipeMenu.vue
@@ -30,14 +30,26 @@ export default {
       touchTime: 0,
       currentPageA: 1,
       buttonsA: [
-        {name: '菜单1'},
-        {name: '菜单2'},
-        {name: '菜单3'},
-        {name: '菜单4'},
+        {name: 'メニュー１'},
+        {name: 'メニュー２'},
+        {name: 'メニュー３'},
+        {name: 'メニュー４'},
       ],
       currentPageB: 1,
       buttonsB: [],
     };
+  },
+  created () {
+    let count = 0;
+    while (count < 50) {
+      this.buttonsB.push({
+        color: '#' +
+            ('00' + Math.floor(Math.random()*Math.floor(255)).toString(16)).slice(-2) +
+            ('00' + Math.floor(Math.random()*Math.floor(255)).toString(16)).slice(-2) +
+            ('00' + Math.floor(Math.random()*Math.floor(255)).toString(16)).slice(-2)
+      });
+      count++;
+    }
   },
   methods: {
     onButtonClickA: function (page) {
@@ -78,5 +90,37 @@ export default {
 </script>
 
 <style scoped>
+.screen {
+  font-size: 30px;
+  text-align: center;
+  width: 100%;
+  height: 200px;
+  line-height: 200px;
+}
+.buttons {
+  /*width: 100%;*/
+  background-color: rgba(0, 0, 0, 0.5);
+  overflow: hidden;
 
+}
+.buttons button {
+  width: 10rem;
+  margin: .5rem 2rem;
+  height: 40px;
+  border-radius: .5rem;
+  color: rgb(128, 127, 128);
+  background-color: #f0f0f0;
+}
+
+.buttons button.active {
+  color: #000;
+  background-color: #fef;
+}
+.buttons button.btn-color {
+  width: 50px;
+  margin: 0;
+  padding: 0;
+  border: 1px solid black;
+  font-size: 10px;
+}
 </style>
