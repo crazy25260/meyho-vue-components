c6c48f4c837aa9bbd79461ccc409854049820593
zhangrui <zhangrui6@asiainfo.com>
2021-04-09 17:55:01 +0800

    终于TMD搞定了。 插件发布  带着2个组件。

---
+++ meyho-vue-swipe-menu/src/plugins/SingleCom/index.js
@@ -0,0 +1,10 @@
+// 导入组件，组件必须声明name
+import SingleCom from "@/components/SingleCom"
+
+// 为组件提供install安装方法，供按需引入
+SingleCom.install = function (Vue) {
+    Vue.component(SingleCom.name, SingleCom)
+}
+
+// 默认导出组件
+export default SingleCom
--- meyho-vue-swipe-menu/src/plugins/index.js
+++ meyho-vue-swipe-menu/src/plugins/index.js
@@ -1,29 +1,23 @@
 // 导入组件，组件必须声明name
-import SwipeMenu from "./swipe-menu"
-import SingleCom from "@/components/SingleCom";
-const components = [ SwipeMenu, SingleCom ]
+import MyMenu from "@/plugins/MeyhoSwipeMenu"
+import SingleCom from "@/plugins/SingleCom";
+const components = [ MyMenu, SingleCom ]
 
 const install = function(Vue) {
     // 判断是否安装
     if (install.installed) return // 遍历注册全局组件
     components.map(component => Vue.component(component.name, component))
-    install.installed = true;
 }
 
 const plugin = {
     // 导出的对象必须具有install，才能被Vue.use()方法安装
     install,
-    SwipeMenu,
     SingleCom
 };
 
 // 判断是否直接引入文件
 if (typeof window !== "undefined" && window.Vue) {
     install(window.Vue)
-}
-
-if (window.Vue) {
-    window.Vue.use(plugin);
 }
 
 // 默认导出组件
