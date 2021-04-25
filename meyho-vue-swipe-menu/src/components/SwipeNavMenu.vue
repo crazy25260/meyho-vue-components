67e56a4b13e725f69eb38834d145ec75246246df
zhangrui <zhangrui6@asiainfo.com>
2021-04-25 10:31:35 +0800

    add

---
+++ meyho-vue-swipe-menu/src/components/SwipeNavMenu.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+      class="wrapper"
+      ref="wrapper"
+      @touchstart.prevent="onStart"
+      @touchmove.prevent="onMove"
+      @touchend.prevent="onEnd"
+      @touchcancel.prevent="onEnd"
+      @mousedown.prevent="onStart"
+      @mousemove.prevent="onMove"
+      @mouseup.prevent="onEnd"
+      @mousecancel.prevent="onEnd"
+      @mouseleave.prevent="onEnd"
+      @transitionend="onTransitionEnd">
+    <ul
+        class="list"
+        ref="scroller"
+        :style="scrollerStyle">
+      <li
+          class="list-item"
+          v-for="(item, index) in list"
+          :key="index"
+      >
+        {{item}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SwipeNavMenu"
+}
+</script>
+
+<style scoped>
+
+</style>
